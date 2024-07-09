0f8c76c2ba18145da816f779a595ccf7bd74ccf9
astya-min <54018364+astya-min@users.noreply.github.com>
2024-07-09 03:35:34 +0300

    Update settings.py

--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -117,6 +117,9 @@ USE_L10N = True
 
 USE_TZ = True
 
+CSRF_TRUSTED_ORIGINS = ['https://*', 'http://*']
+   
+
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
