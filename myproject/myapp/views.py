68554250f4b5e2cf006f5218f6573052cb3a25cc
astya-min <54018364+astya-min@users.noreply.github.com>
2024-07-10 10:41:19 +0300

    Update views.py

--- myproject/myapp/views.py
+++ myproject/myapp/views.py
@@ -25,7 +25,7 @@ def resize_image(image, width, height):
 def create_image(text, width, height, font, x_position, y_position):
     image = Image.new('RGB', (width, height), color='black')
     draw = ImageDraw.Draw(image)
-    draw.text((x_position, y_position), text.encode('utf-8'), font=font, fill='white')
+    draw.text((x_position, y_position), text, font=font, fill='white')
     return image
 
 
@@ -57,7 +57,7 @@ def generate_video(request):
         duration = 3
         fps = 120
         total_frames = duration * fps
-        font = ImageFont.load_default()
+        font = ImageFont.truetype("DejaVuSans.ttf", 16)
         frames = generate_frames(text, width, height, font, total_frames)
         save_video(frames, filename, fps)
 
